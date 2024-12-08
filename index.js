dfa715a524332e9c6d5c72fff28a3d529ce41fcc
f-khalloufi <155197220+f-khalloufi@users.noreply.github.com>
2024-12-08 15:51:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var cartCount = 0;
 
     // Add item to cart
-    $(".add-to-cart").click(function() {
-        // Increment cart count  
+    $(".add-to-cart").click(function () {
         cartCount++;
-        $("#cart-count p").text(cartCount);
+        updateCartCount();
     });
+
+    // Remove item from cart
+    $(".remove-from-cart").click(function () {
+        if (cartCount > 0) {
+            cartCount--;
+            updateCartCount();
+        } else {
+            alert("Your cart is already empty!");
+        }
+    });
+
+    // Reset cart
+    $("#reset-cart").click(function () {
+        if (cartCount > 0) {
+            cartCount = 0;
+            updateCartCount();
+            alert("Cart has been reset.");
+        } else {
+            alert("Your cart is already empty!");
+        }
+    });
+
+    // Function to update cart count display
+    function updateCartCount() {
+        $("#cart-count p").text(cartCount);
+    }
 });
 
